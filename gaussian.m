900f8417eec2ad13965468faff14b05fca6aa39c
Phoebe <phoebeso@berkeley.edu>
2017-06-30 15:54:22 -0700

    Fixed style

--- gaussian.m
+++ gaussian.m
@@ -1,7 +1,6 @@
-function [ y ] = gaussian( x, sig, c )
+function y = gaussian(x,sig,c)
 % Calculates gaussian of inputed x values given sigma and c values 
 
 y = exp((-(x-c).^2)/(2*(sig^2)));
 
 end
-
